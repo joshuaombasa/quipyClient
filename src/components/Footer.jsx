33cd3e6802aa7253fce5c9d4f2414c822b58b723
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-24 23:27:31 +0300

    added build component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer--container">
+            <p>Ⓒ 2023 quipy IT</p>
+        </div>
+    )
+}
