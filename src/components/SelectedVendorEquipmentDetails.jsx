1ef06569486b2af064ef7f5a0667aad68aae0a04
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-29 00:04:01 +0300

    add build component

---
+++ src/components/SelectedVendorEquipmentDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useOutletContext } from "react-router-dom";
+
+export default function SelectedVendorEquipmentDetails() {
+
+    const { machine } = useOutletContext()
+
+    return (
+        <div className="selectedVendorEquipment--details">
+           <p><strong>Name:</strong>{machine.name}</p>
+           <p><strong>Type:</strong>{machine.type}</p>
+           <p><strong>Description: </strong>{machine.description}</p>
+        </div>
+    )
+}
