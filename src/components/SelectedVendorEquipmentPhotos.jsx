8a622eb22be775f45edbce12a898733ea94b17bd
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-29 00:03:46 +0300

    add build component

---
+++ src/components/SelectedVendorEquipmentPhotos.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import spacePhoto from '../assets/space.jpg'
+import { useOutletContext } from "react-router-dom";
+
+
+export default function SelectedVendorEquipmentPhotos() {
+
+    const { machine } = useOutletContext()
+    
+    return (
+        <div className="selectedVendorEquipment--photos">
+            <img src={spacePhoto} alt="" />
+        </div>
+    )
+}
