464f26c68fbee533332a4df7517b0da9092355de
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-29 00:03:15 +0300

    add build component

--- src/components/SingleMachineItem.jsx
+++ src/components/SingleMachineItem.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function SingleMachineItem() {
+export default function SingleMachineItem({machine,filterType}) {
+
+    
     return (
-        <div className="singleMachineItem">
+        <Link  to={`${machine.id}`} className="singleMachineItem" state={{filterType:filterType}}>
             <div className="singleMachineItem--container">
                 <img src="https://t3.ftcdn.net/jpg/01/27/96/46/240_F_127964695_WxIo3nLZmbwMLCd8jWQdr7fl1KfH1L6N.jpg" alt="" />
                 <div className="singleMachineItem--info">
-                    <h3>CT Scanner</h3>
+                    <h3>{machine.name}</h3>
                     <h3 className="singleMachineItem--price">
-                        $900/month
+                        ${machine.price}/month
                     </h3>
                 </div>
-                <span className="singleMachineItem--type">Laboratory</span>
+                <span className="singleMachineItem--type">{machine.type}</span>
             </div>
-        </div>
+        </Link>
     )
 }
