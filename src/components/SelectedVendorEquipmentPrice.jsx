c536887bb9f7de6206bbdfe69859647cd915ced1
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-29 00:03:32 +0300

    add build component

---
+++ src/components/SelectedVendorEquipmentPrice.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useOutletContext } from "react-router-dom";
+
+export default function SelectedVendorEquipmentPrice() {
+
+    const { machine } = useOutletContext()
+    
+    return (
+        <div className="selectedVendorEquipment--price">
+          <p><strong>Price:</strong></p>
+        </div>
+    )
+}
