b7b6018e268441590a4066ba4137b07048daf982
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-29 00:01:40 +0300

    updated build page

--- src/pages/SingleMachine.jsx
+++ src/pages/SingleMachine.jsx
@@ -1,23 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams, useLocation } from "react-router-dom";
 
 
 export default function SingleMachine() {
+
+    const location = useLocation()
+
+    const params = useParams()
+    const [machine, setMachine] = useState([])
+
+    useEffect(() => {
+        async function fetchMachine() {
+            const res = await fetch(`http://localhost:4000/api/equipment/${params.id}`)
+            const data = await res.json()
+            setMachine(data)
+        }
+
+        fetchMachine()
+    },[])
+
+    const prevSearchParams = location.state ? location.state.filterType : 'all'
+
+    const backLinkLogic = prevSearchParams !== 'all' ? `?type=${prevSearchParams}`: ``
+
     return (
         <div className="single--machine--page">
-
             <div className="single--machine--page--container">
                 <Link
                     className="back--link"
-                    to='..'
+                    to={`..${backLinkLogic}`}
                     relative="path"
-                >&larr; Back to all equipment</Link>
+                >&larr; Back to {prevSearchParams} equipment</Link>
                 <img src="https://t3.ftcdn.net/jpg/01/27/96/46/240_F_127964695_WxIo3nLZmbwMLCd8jWQdr7fl1KfH1L6N.jpg" alt="" />
-                <h1>MRI Scanner</h1>
-                <span className="singleMachineItem--type">Diagnostic</span>
-                <h3>$900/month</h3>
-                <p>The Philips Ingenuity CT series is known for its advanced imaging capabilities and user-friendly design. It offers fast and accurate imaging with reduced artifacts, providing detailed and reliable diagnostic information. With features such as metal artifact reduction and spectral imaging, it is suitable for various complex imaging procedures, including trauma assessment and oncology staging.</p>
-                <Link className="equipment--lease-link">Lease this equipment</Link>
+                <h1>{machine.name}</h1>
+                <span className="singleMachineItem--type">{machine.type}</span>
+                <h3>${machine.price}/month</h3>
+                <p>{machine.description}</p>
+                <Link to='.' className="equipment--lease-link">Lease this equipment</Link>
             </div>
         </div>
     )
