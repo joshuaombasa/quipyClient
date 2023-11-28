dd87f167b47245688f7afea2e02ee0632834db3d
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-29 00:02:16 +0300

    updated build page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
                 <h1>Welcome to Quipy – Your Trusted Partner in Medical Equipment Leasing!</h1>
                 <p>At Quipy, we understand the critical importance of having reliable and state-of-the-art medical equipment to ensure top-notch patient care. As a leading provider of medical equipment leasing solutions, we are dedicated to simplifying the process for healthcare professionals, clinics, and institutions.</p>
                 <p>Partner with Quipy today and elevate your healthcare services with the latest medical equipment. Focus on what matters most – providing exceptional patient care – while we take care of your equipment leasing needs.</p>
-                <Link>See our equipment</Link>
+                <Link to='/equipment'>See our equipment</Link>
             </div>
         </div>
     )
