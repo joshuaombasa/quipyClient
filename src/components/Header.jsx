513f9a7d6356ed7cf7ef5c717f03df216674ad19
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-30 16:45:10 +0300

    added build component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,32 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
+
+    const activeStyle = {
+        border: '2px solid #fff'
+    }
+
     return (
         <div className="header--container--parent">
             <div className="header--container">
                 <Link to='/' className="logo">quipy</Link>
                 <div className="header--nav">
-                    <NavLink to='/about'>About</NavLink>
-                    <NavLink to='/vendor'>Vendor</NavLink>
-                    <NavLink to='/equipment'>Equipment</NavLink>
-                    <NavLink>Logout</NavLink>
+                    <NavLink 
+                       style={({isActive}) => isActive ? activeStyle : null}
+                       to='/about'
+                    >About</NavLink>
+                    <NavLink 
+                       style={({isActive}) => isActive ? activeStyle : null}
+                       to='/vendor'
+                    >Vendor</NavLink>
+                    <NavLink 
+                       style={({isActive}) => isActive ? activeStyle : null}
+                       to='/equipment'
+                    >Equipment</NavLink>
+                    <NavLink
+                       to='/logout'
+                       style={({isActive}) => isActive ? activeStyle : null}
+                    >Logout</NavLink>
                 </div>
             </div>
         </div>
