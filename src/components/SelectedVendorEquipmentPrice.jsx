a1723be4b24ba634d1b4d76e274445857211ebe1
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-30 16:44:24 +0300

    added build component

--- src/components/SelectedVendorEquipmentPrice.jsx
+++ src/components/SelectedVendorEquipmentPrice.jsx
@@ -4,10 +4,10 @@ import { useOutletContext } from "react-router-dom";
 export default function SelectedVendorEquipmentPrice() {
 
     const { machine } = useOutletContext()
-    
+
     return (
         <div className="selectedVendorEquipment--price">
-          <p><strong>Price:</strong></p>
+          <p><strong>Price: </strong>${machine.price}/month</p>
         </div>
     )
 }
