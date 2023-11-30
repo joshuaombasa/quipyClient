499bf35e86b6b1ae57f48fb3c92ee30424e12e6d
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-30 16:43:50 +0300

    added build component

--- src/components/VendorLayout.jsx
+++ src/components/VendorLayout.jsx
@@ -2,14 +2,32 @@ import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function VendorLayout() {
+
+    const activeStyle = {
+        border: '2px solid #4D4D4D'
+    }
+
     return (
         <div className="vendor--layout--parent">
             <div className="vendor--layout--container">
                 <div className="vendor--layout--nav">
-                    <NavLink>Dashboard</NavLink>
-                    <NavLink to='income'>Income</NavLink>
-                    <NavLink to='equipment'>Equipment</NavLink>
-                    <NavLink to='reviews'>Reviews</NavLink>
+                    <NavLink
+                       to='.'
+                       style={({isActive}) => isActive ? activeStyle : null }
+                       end
+                    >Dashboard</NavLink>
+                    <NavLink
+                       style={({isActive}) => isActive ? activeStyle : null } 
+                       to='income'
+                    >Income</NavLink>
+                    <NavLink
+                       style={({isActive}) => isActive ? activeStyle : null } 
+                       to='equipment'
+                    >Equipment</NavLink>
+                    <NavLink
+                       style={({isActive}) => isActive ? activeStyle : null } 
+                       to='reviews'
+                    >Reviews</NavLink>
                 </div>
                 <Outlet/>
             </div>
