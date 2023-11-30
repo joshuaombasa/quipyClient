c5bedda726d0dca1357b2171c401adefc01eb29d
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-11-30 16:44:40 +0300

    added build component

--- src/components/SelectedVendorEquipmentDetails.jsx
+++ src/components/SelectedVendorEquipmentDetails.jsx
@@ -7,8 +7,8 @@ export default function SelectedVendorEquipmentDetails() {
 
     return (
         <div className="selectedVendorEquipment--details">
-           <p><strong>Name:</strong>{machine.name}</p>
-           <p><strong>Type:</strong>{machine.type}</p>
+           <p><strong>Name: </strong>{machine.name}</p>
+           <p><strong>Type: </strong>{machine.type}</p>
            <p><strong>Description: </strong>{machine.description}</p>
         </div>
     )
