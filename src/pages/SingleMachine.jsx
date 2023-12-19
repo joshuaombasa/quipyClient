f9ef5f5e6dab60abac0c2602b0b9df29707c0043
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-19 15:33:20 +0300

    updated build page

--- src/pages/SingleMachine.jsx
+++ src/pages/SingleMachine.jsx
@@ -1,31 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams, useLocation } from "react-router-dom";
+import React, { Suspense, useEffect, useState } from "react";
+import { Link, useParams, useLocation, defer, useLoaderData , Await} from "react-router-dom";
+import { getMachine } from "../utils/api";
+
+export async function loader({ request, params }) {
+    return defer({ machine: getMachine(params.id) })
+}
 
 
 export default function SingleMachine() {
 
+    const dataPromise = useLoaderData()
+
     const location = useLocation()
 
-    const params = useParams()
-    const [machine, setMachine] = useState([])
+    const prevSearchParams = location.state.filterType ? location.state.filterType : 'all'
 
-    useEffect(() => {
-        async function fetchMachine() {
-            const res = await fetch(`http://localhost:4000/api/equipment/${params.id}`)
-            const data = await res.json()
-            setMachine(data)
-        }
+    const backLinkLogic = prevSearchParams !== 'all' ? `?type=${prevSearchParams}` : ``
 
-        fetchMachine()
-    },[])
-
-    const prevSearchParams = location.state ? location.state.filterType : 'all'
-
-    const backLinkLogic = prevSearchParams !== 'all' ? `?type=${prevSearchParams}`: ``
-
-    return (
-        <div className="single--machine--page">
-            <div className="single--machine--page--container">
+    function renderMachine(machine) {
+        return (
+            <>
                 <Link
                     className="back--link"
                     to={`..${backLinkLogic}`}
@@ -37,6 +31,18 @@ export default function SingleMachine() {
                 <h3>${machine.price}/month</h3>
                 <p>{machine.description}</p>
                 <Link to='.' className="equipment--lease-link">Lease this equipment</Link>
+            </>
+        )
+    }
+
+    return (
+        <div className="single--machine--page">
+            <div className="single--machine--page--container">
+                <Suspense fallback={<h1>Loading...</h1>}>
+                    <Await resolve={dataPromise.machine}>
+                        {renderMachine}
+                    </Await>
+                </Suspense>
             </div>
         </div>
     )
