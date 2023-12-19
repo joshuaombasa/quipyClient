335c02fed0f9ed09a057263968ea509b3def05aa
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-19 15:33:43 +0300

    updated build page

--- src/pages/vendor/VendorEquipment.jsx
+++ src/pages/vendor/VendorEquipment.jsx
@@ -1,43 +1,44 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import spaceImg from '../../assets/space.jpg'
+import React, { Suspense, useEffect, useState } from "react";
+import { Await, defer, Link, useLoaderData } from "react-router-dom";
+import { getVendorMachines } from "../../utils/api";
+
+export async function loader() {
+    return defer({ machines: getVendorMachines() })
+}
 
 export default function VendorEquipment() {
 
-    const [vendorEquipment, setVendorEquipment] = useState([])
+    const dataPromise = useLoaderData()
 
-    useEffect(() => {
-        async function fetchVendorEquipment() {
-            const res = await fetch('http://localhost:4000/api/vendorEquipment')
-            const data = await res.json()
-            setVendorEquipment(data)
-        }
+    function renderVendorEquipment(vendorEquipment) {
 
-        fetchVendorEquipment()
-    }, [])
+        const vendorEquipmentElements = vendorEquipment.map(item => (
+            <Link to={item.id} key={item.id} className='vendorEquipmentItem'>
+                <img src="https://t3.ftcdn.net/jpg/01/27/96/46/240_F_127964695_WxIo3nLZmbwMLCd8jWQdr7fl1KfH1L6N.jpg" alt="" />
+                <div className="vendor--equipment--text">
+                    <h3>{item.name}</h3>
+                    <span className="vendor--equipment--type">{item.type}</span>
+                    <span className="vendor--equipment--price">${item.price}/month</span>
+                </div>
+            </Link>
+        ))
 
-    if (vendorEquipment.length === 0) {
         return (
-            <h1>Loading...</h1>
+            <>
+                <h1>Your listed Equipment:</h1>
+                {vendorEquipmentElements}
+            </>
         )
     }
-
-    const vendorEquipmentElements = vendorEquipment.map(item => (
-        <Link to={item.id} key={item.id} className='vendorEquipmentItem'>
-            <img src="https://t3.ftcdn.net/jpg/01/27/96/46/240_F_127964695_WxIo3nLZmbwMLCd8jWQdr7fl1KfH1L6N.jpg" alt="" />
-            <div className="vendor--equipment--text">
-                <h3>{item.name}</h3>
-                <span className="vendor--equipment--type">{item.type}</span>
-                <span className="vendor--equipment--price">${item.price}/month</span>
-            </div>
-        </Link>
-    ))
 
     return (
         <div className="vendorEquipment--page">
             <div className="vendorEquipment--page--container">
-                <h1>This is VendorEquipment</h1>
-                {vendorEquipmentElements}
+                <Suspense return={<h1>Loading...</h1>}>
+                    <Await resolve={dataPromise.machines}>
+                        {renderVendorEquipment}
+                    </Await>
+                </Suspense>
             </div>
         </div>
     )
