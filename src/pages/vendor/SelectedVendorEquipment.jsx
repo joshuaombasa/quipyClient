0a452693bb9168a982ba767b829d47828c6dd4ad
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-19 15:34:03 +0300

    updated build page

--- src/pages/vendor/SelectedVendorEquipment.jsx
+++ src/pages/vendor/SelectedVendorEquipment.jsx
@@ -1,35 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { NavLink, Outlet, useParams } from "react-router-dom";
+import React, { Suspense, useEffect, useState } from "react";
+import { Await, defer, NavLink, Outlet, useLoaderData, useParams } from "react-router-dom";
+import { getMachine } from "../../utils/api";
+
+// export async function loader({ request, params }) {
+//     console.log('Wow')
+//     return defer({machine: getMachine(params.id)})
+// }
+
+export async function loader({ request, params }) {
+    return defer({ machine: getMachine(params.id) })
+}
 
 export default function SelectedVendorEquipment() {
 
-    const params = useParams()
-
-    const [machine, setMachine] = useState(null)
-
-    useEffect(() => {
-        async function fetchMachine() {
-            const res = await fetch(`http://localhost:4000/api/equipment/${params.id}`)
-            const data = await res.json()
-            setMachine(data)
-        }
-
-        fetchMachine()
-    }, [])
-
-    if (!machine) {
-        return (
-            <h1>Loading...</h1>
-        )
-    }
+    const dataPromise = useLoaderData()
 
     const activeStyle = {
         border: '2px solid #4D4D4D'
     }
 
-    return (
-        <div className="selectedVendorEquipment--page">
-            <div className="selectedVendorEquipment--page--container">
+    function renderSelectedVendorEquipment(machine) {
+        return (
+            <>
                 <div className="top--area">
                     <img src={machine.imageUrl} alt="" />
                     <div className="selectedVendorEquipment--top--right">
@@ -38,21 +30,33 @@ export default function SelectedVendorEquipment() {
                     </div>
                 </div>
                 <div className="selectedVendorEquipment--page--nav">
-                    <NavLink 
-                         to='.'
-                         style={({isActive}) => isActive ? activeStyle: null}
-                         end
+                    <NavLink
+                        to='.'
+                        style={({ isActive }) => isActive ? activeStyle : null}
+                        end
                     >Details</NavLink>
-                    <NavLink 
-                         to='price'
-                         style={({isActive}) => isActive ? activeStyle: null}
+                    <NavLink
+                        to='price'
+                        style={({ isActive }) => isActive ? activeStyle : null}
                     >Price</NavLink>
-                    <NavLink 
-                         to='photos'
-                         style={({isActive}) => isActive ? activeStyle: null}
+                    <NavLink
+                        to='photos'
+                        style={({ isActive }) => isActive ? activeStyle : null}
                     >Images</NavLink>
                 </div>
                 <Outlet context={{ machine }} />
+            </>
+        )
+    }
+
+    return (
+        <div className="selectedVendorEquipment--page">
+            <div className="selectedVendorEquipment--page--container">
+                <Suspense fallback={<h1>Loading...</h1>}>
+                    <Await resolve={dataPromise.machine}>
+                        {renderSelectedVendorEquipment}
+                    </Await>
+                </Suspense>
             </div>
         </div>
     )
